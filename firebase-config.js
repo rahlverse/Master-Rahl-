38482f93d0ace6768b5f88ebfeaaf8be465a858d
rahlverse <p99610908@gmail.com>
2025-07-01 20:56:21 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// RAHL LAUNCHPAD - Firebase Config (Ready for use)
+export const firebaseConfig = {
+  apiKey: "AIzaSyD6bvr1VsG2vhVFwT3pmAtcIvadrAlIO7Y",
+  authDomain: "rahlverse-empire.firebaseapp.com",
+  projectId: "rahlverse-empire",
+  storageBucket: "rahlverse-empire.firebasestorage.app",
+  messagingSenderId: "519945468450",
+  appId: "1:519945468450:web:789cdf8830bc31e83ba87b",
+  measurementId: "G-2K024KSR5M"
+};
