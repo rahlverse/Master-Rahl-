a7485f996b9ac053e917b5164745f317ba80cc39
rahlverse <p99610908@gmail.com>
2025-07-01 21:45:19 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,12 @@
-// RAHL LAUNCHPAD - Firebase Config (Ready for use)
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyD6bvr1VsG2vhVFwT3pmAtcIvadrAlIO7Y",
   authDomain: "rahlverse-empire.firebaseapp.com",
   projectId: "rahlverse-empire",
-  storageBucket: "rahlverse-empire.firebasestorage.app",
+  storageBucket: "rahlverse-empire.appspot.com",
   messagingSenderId: "519945468450",
   appId: "1:519945468450:web:789cdf8830bc31e83ba87b",
   measurementId: "G-2K024KSR5M"
 };
+
+// ✅ Export it like this:
+export { firebaseConfig };
